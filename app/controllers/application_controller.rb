36e236aea408c6de0bebbff6446228c86dd6e696
narayankumbhakar123 <narayankumbhakar@gmail.com>
2024-05-20 18:56:00 +0530

    remove authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,2 @@
 class ApplicationController < ActionController::Base
-    include JsonWebToken
-    # skip_before_action :verify_authenticity_token
-    # before_action :authenticate_request
-
-    private
-
-    def authenticate_request
-        header = request.headers["Authorization"]
-        header = header.split(" ").last if header
-    begin
-        decoded = jwt_decode(header)
-        @current_user = User.find(decoded[:user_id])
-    rescue ActiveRecord::RecordNotFound => e
-        render json: { errors: e.message }, status: :unauthorized
-    rescue JWT::DecodeError => e
-        render json: { errors: e.message }, status: :unauthorized
-    end
-    end
 end
