07ae9bf23d295826a41aeff88af3135c732a6fc1
narayankumbhakar123 <narayankumbhakar@gmail.com>
2024-05-21 12:30:57 +0530

    updated seed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  root to: 'admin/dashboard#index', as: :custom_admin_root
 
   resources :users
   resources :webhooks
